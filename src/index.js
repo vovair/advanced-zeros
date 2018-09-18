0ba69f35817ee8790410b98b8aea66bf697d2d0b
vovair <vovair@gmail.com>
2018-09-18 14:54:27 +0300

    Added implementation of getZerosCount.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  const getMaxPower = (p, n) => {
+    let power = 0;
+    while (n / p > 0) {
+      power += Math.floor(n / p);
+      n /= p;
+    }
+    return power;
+  };
+
+  const getMaxPowerByQ = (p, n, q) => {
+    return Math.floor(getMaxPower(p, n) / q);
+  };
+
+  const processBase = (currentBase, iterBase) => {
+    let q = 0;
+    while (currentBase % iterBase == 0) {
+      q++;
+      currentBase /= iterBase;
+    }
+    return {q, b: currentBase};
+  };
+
+  let result = number;
+  let j = base;
+  for (let p = 2; p <= base; p++) {
+    if (j % p != 0) continue;
+    const {q, b} = processBase(j, p);
+    j = b;
+    result = Math.min(result, getMaxPowerByQ(p, number, q));
+  }
+  return result;
+};
